b524fa59b12e5d360cf7f8ab0cf8945598eb63b9
Riyard20 <kamarudeenriyad2@gmail.com>
2024-11-16 08:48:54 -0600

    done

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace SyntaxAndSyntaxSugarExercise
+﻿using System.ComponentModel.Design;
+
+namespace SyntaxAndSyntaxSugarExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 10;
+            var reponse = answer < 9? $"{answer} is less than nine" : $"{answer} is greater than nine or equal to 9";
+            Console.WriteLine(reponse);
+            //if (answer < 9)
+            //{
+                //reponse = answer + "is less than nine";
+                
+            //}
+
+            //{
+              //  else 
+            
         }
     }
 }
